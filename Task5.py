645a707b63453a24648137c40bc915ee61612e81
NPozn <nikitos.orenburg@yandex.ru>
2016-12-23 02:51:48 +0600

    Задание № 5 Добавлен пробел

--- Task5.py
+++ Task5.py
@@ -3,7 +3,7 @@ import re
 def ContainSymbols(s):
     return re.findall(r"[wrtpsdfghjklzxcvbnmцкнтглдшщзхфвпртсчб]", s) != [] and re.findall(r"[-]", s) == []
 
-a = re.split(r";|,|:|!",input("Введите предложение: "))
+a = re.split(r";|,|:|!|\s+",input("Введите предложение: "))
 print("Слова с согласными буквами :")
 for i in range(len(a)):
     if ContainSymbols(a[i]) == True:
