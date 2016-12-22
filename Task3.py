d4f473348b458cab9e552897b544aab4eccffa49
NPozn <nikitos.orenburg@yandex.ru>
2016-12-23 03:27:56 +0600

    Задание № 3 Доделано

--- Task3.py
+++ Task3.py
@@ -10,14 +10,16 @@ def Plus(first, second):
 
 def Multip(first, second):
     res = []
+    temp = []
     for i in range(len(first)):
-        temp = []
         sum = 0
+        temp =[]
         for j in range(len(first[i])):
             for k in range(len(second[j])):
-                sum += first[i][j] * second[k][j]
-        temp.append(sum)
+                sum = sum +(first[i][j] * second[k][i])
+            temp.append(sum)
         res.append(temp)
+        #print(temp)
     return res
 
 def PrintMatrix(matrix):
@@ -27,7 +29,7 @@ def PrintMatrix(matrix):
         print("\n")
     print()
 
-matrix1 = [[1, 2, 5 ,6], [4, 6, 9 ,2],[4,1,2,3],[8,7,6,4]]
+matrix1 = [[1, 2, 5 , 6], [4, 6, 9 ,2],[4,1,2,3],[8,7,6,4]]
 matrix2 = [[3, 4, 8, 12],[7, 10 ,11 ,23],[8,7,6,4],[4,1,2,3]]
 #PrintMatrix(Plus(matrix1,matrix2))
 PrintMatrix(Multip(matrix1,matrix2))
