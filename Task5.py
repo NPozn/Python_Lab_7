f8f595075eb64e825bccb52c3319feaa305193ec
NPozn <nikitos.orenburg@yandex.ru>
2016-12-23 02:43:55 +0600

    Задание № 5 Сделано

--- Task5.py
+++ Task5.py
@@ -3,9 +3,8 @@ import re
 def ContainSymbols(s):
     return re.findall(r"[wrtpsdfghjklzxcvbnmцкнтглдшщзхфвпртсчб]", s) != [] and re.findall(r"[-]", s) == []
 
-input_data=input("Введите предложение: ").split(",",maxsplit=-1)
-
-for i in range(len(input_data)):
-    if(ContainSymbols(input_data[i])==True):
-        print(input_data)
-
+a = re.split(r";|,|:|!",input("Введите предложение: "))
+print("Слова с согласными буквами :")
+for i in range(len(a)):
+    if ContainSymbols(a[i]) == True:
+        print(a[i])
